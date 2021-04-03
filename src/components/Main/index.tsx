1869bcb0af1e502e87a1d5dd0eee60349b2cbd05
Mauricio <mromauricio@gmail.com>
2021-04-03 18:26:01 -0300

    boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
-const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="img/hero-illustration.svg" />
+  </S.Wrapper>
 )
 
 export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
-import Main from 'components/Main'
+import * as S from '../components/Main/styles'
 
 export default function Home() {
-  return <Main />
+  return (
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, RactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="img/hero-illustration.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+    </S.Wrapper>
+  )
 }
